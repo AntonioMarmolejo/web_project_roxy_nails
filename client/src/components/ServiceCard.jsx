f49011487347f49cad9ee8e5ed038b4e1b1909e9
Antonio Marmolejo <djcamg71@gmail.com>
2026-07-23 04:29:44 -0500

    feat: se hacen mas grandes la imágenes de los servicios para destacarlos

--- client/src/components/ServiceCard.jsx
+++ client/src/components/ServiceCard.jsx
@@ -15,31 +15,33 @@ export default function ServiceCard({ service, onClick }) {
 
     return (
         <div className="service-card" onClick={() => onClick ? onClick(service) : navigate('/agendar')}>
-            <div className="service-card__icon">
-                {service.image ? <img src={service.image} alt={service.name} className="service-card__image" /> : icon}
+            <div className="service-card__image">
+                {service.image ? <img src={service.image} alt={service.name} /> : icon}
             </div>
 
-            <div className="service-card__name">
-                {service.name}
-            </div>
-
-            {service.description && (
-                <div className="service-card__desc">
-                    {service.description}
+            <div className="service-card__body">
+                <div className="service-card__name">
+                    {service.name}
                 </div>
-            )}
 
-            <div className="service-card__price">
-                desde ${service.price}
+                {service.description && (
+                    <div className="service-card__desc">
+                        {service.description}
+                    </div>
+                )}
+
+                <div className="service-card__price">
+                    desde ${service.price}
+                </div>
+
+                <div className="service-card__duration">
+                    {service.duration} min
+                </div>
+
+                {service.featured && (
+                    <span className="service-card__featured">⭐ Destacado</span>
+                )}
             </div>
-
-            <div className="service-card__duration">
-                {service.duration} min
-            </div>
-
-            {service.featured && (
-                <span className="service-card__featured">⭐ Destacado</span>
-            )}
         </div>
     )
 }
