a4d18a2efad375877a375022ad60508622078b8f
Antonio Marmolejo <djcamg71@gmail.com>
2026-07-24 02:46:09 -0500

    fix: se mejora el diseño de las tarjetas en servicios

--- client/src/components/ServiceCard.jsx
+++ client/src/components/ServiceCard.jsx
@@ -17,6 +17,9 @@ export default function ServiceCard({ service, onClick }) {
         <div className="service-card" onClick={() => onClick ? onClick(service) : navigate('/agendar')}>
             <div className="service-card__image">
                 {service.image ? <img src={service.image} alt={service.name} /> : icon}
+                {service.featured && (
+                    <span className="service-card__featured">⭐ Destacado</span>
+                )}
             </div>
 
             <div className="service-card__body">
@@ -30,17 +33,14 @@ export default function ServiceCard({ service, onClick }) {
                     </div>
                 )}
 
-                <div className="service-card__price">
-                    desde ${service.price}
+                <div className="service-card__footer">
+                    <span className="service-card__price">
+                        desde ${service.price}
+                    </span>
+                    <span className="service-card__duration">
+                        {service.duration} min
+                    </span>
                 </div>
-
-                <div className="service-card__duration">
-                    {service.duration} min
-                </div>
-
-                {service.featured && (
-                    <span className="service-card__featured">⭐ Destacado</span>
-                )}
             </div>
         </div>
     )
